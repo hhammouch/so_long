3a6eb89c5ec755e8c275fa1f2a1a5e531b87820f
Hamza Hammouch <hamzapc212@gmail.com>
2025-03-22 02:07:14 +0000

    🍟

--- game.c
+++ game.c
@@ -6,7 +6,7 @@
 /*   By: hhammouc <hhammouc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/22 00:04:25 by hhammouc          #+#    #+#             */
-/*   Updated: 2025/03/22 01:03:11 by hhammouc         ###   ########.fr       */
+/*   Updated: 2025/03/22 01:18:05 by hhammouc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,9 @@ void	load_textures(t_game *game)
 	size = game->tile_size;
 	game->wall = mlx_xpm_file_to_image(game->mlx, "rec/wall.xpm", &size, &size);
 	game->player = mlx_xpm_file_to_image(game->mlx, "rec/player.xpm", &size, &size);
-	game->collectible = mlx_xpm_file_to_image(game->mlx, "textures/collectible.xpm",&size, &size);
-	game->exit = mlx_xpm_file_to_image(game->mlx, "textures/exit.xpm", &size, &size);
-	game->empty = mlx_xpm_file_to_image(game->mlx, "textures/empty.xpm", &size,&size);
+	game->collectible = mlx_xpm_file_to_image(game->mlx, "rec/collectible.xpm",&size, &size);
+	game->exit = mlx_xpm_file_to_image(game->mlx, "rec/exit.xpm", &size, &size);
+	game->empty = mlx_xpm_file_to_image(game->mlx, "rec/empty.xpm", &size,&size);
 	if (!game->wall || !game->empty || !game->player || !game->exit || !game->collectible)
 		print_error("Failed to load textures.");
 }
